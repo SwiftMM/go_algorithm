feedcd30cd8a8bd329c11afb79012cf89a045e12
caoxz <caoxz@ap-ec.cn>
2019-05-13 11:57:18 +0800

    add "remove_duplicates"

---
+++ go_algorithm/Knumber/calculate_k.go
@@ -0,0 +1,17 @@
+package Knumber
+
+// K数，无序数组，某个数他的左边的所有数比他小，他右边的所有数比他大，他就是K数。K数不止一个。
+// 例如[7, 4, 2, 8, 9, 12]，8，9是K数。
+
+func FindK(nums []int) []int {
+	length := len(nums)
+	res := []int{}
+
+	for index := range nums {
+
+		i, j := index+1, length-1
+
+	}
+
+	return res
+}
